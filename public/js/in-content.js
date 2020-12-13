ec73b4d96781521089dcf501fec136e532ee53ad
sergey.vashkevich <sergey.vashkevich@masterdata.ru>
2020-12-13 21:07:48 +0000

    structure for launch two different apps

--- public/js/in-content.js
+++ public/js/in-content.js
@@ -5,26 +5,7 @@
 * Note that in this scenario the port is open from the popup, but other extensions may open it from the background page or not even have either background.js or popup.js.
 * */
 
-
-// Avoid recursive frame insertion...
-var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
-// eslint-disable-next-line no-restricted-globals
-if (!location.ancestorOrigins.contains(extensionOrigin)) {
-    var iframe = document.createElement('iframe');
-    // Must be declared at web_accessible_resources in manifest.json
-    // eslint-disable-next-line no-undef
-    iframe.srcdoc = chrome.runtime.getURL('index.html').get().replace('popup', "inner");
-    // Some styles for a fancy sidebar
-    iframe.style.cssText = 'position:fixed;top:0;right:0;display:block;' +
-        'width:300px;height:300px;z-index:1000;';
-    document.body.appendChild(iframe);
-}
-
-
-
-
-
-
+insertMapApp();
 
 // Extension port to communicate with the popup, also helps detecting when it closes
 let port = null;
@@ -55,3 +36,19 @@ const handleBackgroundResponse = response =>
 
 // Send a message to background.js
 chrome.runtime.sendMessage('Message from in-content.js!', handleBackgroundResponse);
+
+function insertMapApp() {
+    // Avoid recursive frame insertion...
+    var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+    // eslint-disable-next-line no-restricted-globals
+    if (!location.ancestorOrigins.contains(extensionOrigin)) {
+        var iframe = document.createElement('iframe');
+        // Must be declared at web_accessible_resources in manifest.json
+        // eslint-disable-next-line no-undef
+        iframe.src = chrome.runtime.getURL('index.html');
+        // Some styles for a fancy sidebar
+        iframe.style.cssText = 'position:fixed;top:0;right:0;display:block;' +
+            'width:300px;height:300px;z-index:1000;';
+        document.body.appendChild(iframe);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,20 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import PopupApp from './PopupApp';
+import MapApp from './MapApp';
 import reportWebVitals from './reportWebVitals';
 
-const el = document.getElementById('root').getAttribute('data-param');
-if (el === "popup1") {
+if (inIframe () ) {
+    ReactDOM.render(
+        <React.StrictMode>
+            <MapApp/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+
+} else {
     ReactDOM.render(
         <React.StrictMode>
             <PopupApp/>
         </React.StrictMode>,
         document.getElementById('root')
     );
-} else {
-
 }
-
+function inIframe () {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
